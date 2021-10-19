56f1fef295ad2d3400bcd961202fa000d97319a7
onurkurtulmus <onurk.dev@outlook.com>
2021-10-19 22:02:10 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PrimenumberChecker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int checknumber;
+           
+            Console.WriteLine("Lütfen Asal sayı kontrolü giriniz.");
+            checknumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i < checknumber; i++) {
+
+                if (checknumber % i == 0)
+                {
+
+                    Console.WriteLine($"{checknumber} bir asal sayı Değildir.");
+                    return;
+                }
+        
+
+              
+            }
+
+
+            Console.WriteLine($"{checknumber} bir asal sayı .");
+            Console.ReadKey();
+        }
+    }
+}
